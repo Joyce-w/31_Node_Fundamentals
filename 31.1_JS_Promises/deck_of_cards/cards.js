6b12cef580c454954f88866a92d294963feea713
Joyce <hellojoycewong@gmail.com>
2021-03-06 23:19:36 -0800

    functional promises but err with calling same card every time

--- 31.1_JS_Promises/deck_of_cards/cards.js
+++ 31.1_JS_Promises/deck_of_cards/cards.js
@@ -11,28 +11,22 @@ function newDeck() {
     })
 }
 
-function drawCard() {
+
     newDeck()
         .then((res) => {
             axios.get(`https://deckofcardsapi.com/api/deck/${res.data.deck_id}/draw/?count=1`)
-                .then((res) => {
-                    console.log(res.data.cards[0]['image'])
-                })
+                .then((res) => displayCard(res.data.cards[0].image))
         })
-}
-
-// Not able to get image url back from return to pass into click event!
 
 // jQuery portion
 let btn = $('#more_cards')
 let pile = $('#card_area')
 
 //click event for additional cards
-btn.on('click', function () {
-    let curr_card = $(`<img src=${drawCard}>`)
-    pile.append(curr_card)
-})
+function displayCard(url) {
+    btn.on('click', function () {
+        let curr_card = $(`<img src=${url}>`)
+        pile.append(curr_card)
+    })    
+}
 
-// .then(res => {
-//     console.log(`${res.data.cards[0]['value']} of ${res.data.cards[0]['suit']}`)
-// })
