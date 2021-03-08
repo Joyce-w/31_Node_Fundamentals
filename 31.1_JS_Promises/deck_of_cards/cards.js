b0be72dfc82dd3aec25df96aeb2f1563ae697640
Joyce <hellojoycewong@gmail.com>
2021-03-07 17:32:03 -0800

    functional card draw with button click!

--- 31.1_JS_Promises/deck_of_cards/cards.js
+++ 31.1_JS_Promises/deck_of_cards/cards.js
@@ -2,31 +2,30 @@ let deckID = null;
 let card = null
 
 
-//get card id
-function newDeck() {
-    return new Promise((resolve, reject) => {
-        let res = axios.get('https://deckofcardsapi.com/api/deck/new/draw/?count=1')
-        resolve(res)
-        reject("Something wrong with API")
-    })
-}
+//Make promise to get deck_id
+let newDeck = new Promise((resolve, reject) => {
+    let res = axios.get('https://deckofcardsapi.com/api/deck/new/draw/?count=1')
+    resolve(res)
+    reject("Something wrong with API")
+})
 
-
-    newDeck()
-        .then((res) => {
-            axios.get(`https://deckofcardsapi.com/api/deck/${res.data.deck_id}/draw/?count=1`)
-                .then((res) => displayCard(res.data.cards[0].image))
-        })
 
 // jQuery portion
 let btn = $('#more_cards')
 let pile = $('#card_area')
 
-//click event for additional cards
+//click event for additional cards from newDeck promise
+btn.on('click', function () {
+    newDeck
+        .then((res) => {
+            axios.get(`https://deckofcardsapi.com/api/deck/${res.data.deck_id}/draw/?count=1`)
+                .then((res) => displayCard(res.data.cards[0]['image']))
+        })    
+})
+    
+//display card on screen
 function displayCard(url) {
-    btn.on('click', function () {
-        let curr_card = $(`<img src=${url}>`)
-        pile.append(curr_card)
-    })    
+    let curr_card = $(`<img src=${url}>`)
+    pile.append(curr_card)
 }
 
